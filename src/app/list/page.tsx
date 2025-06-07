1c97fc0caa050f4ff0cbe6015f653d7918f70948
Jiří Müller <jiri.muller.dev@tutamail.com>
2025-06-07 16:51:31 +0200

    Registration list styling

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -3,6 +3,8 @@
 import React from 'react'
 import { db } from '../firebase'
 import { collection, DocumentData, getDocs } from 'firebase/firestore'
+import { format } from 'date-fns'
+import { Timestamp } from 'firebase/firestore'
 
 const RegistarationsList = () => {
   const [documents, setDocuments] = React.useState<Document[]>([])
@@ -11,8 +13,7 @@ const RegistarationsList = () => {
 
   interface Document {
     id: string
-    // Define other fields based on your Firestore document structure
-    name?: string
+    [key: string]: string
   }
 
   React.useEffect(() => {
@@ -52,16 +53,32 @@ const RegistarationsList = () => {
 
   return (
     <main className="flex flex-col items-center">
-      <div className="max-w-screen-md p-6 font-mono">
+      <div className="mt-4 max-w-screen-md bg-white p-6 font-mono">
         <h2>List of registrations:</h2>
-        <ul>
-          {documents.map((doc) => (
-            <li key={doc.id} className="border-b-2 border-b-gray-300 break-all">
-              <strong>ID:</strong> {doc.id} <br />
-              <strong>Data:</strong> {JSON.stringify(doc)}
-            </li>
-          ))}
-        </ul>
+        <section>
+          {documents.map((doc) => {
+            return (
+              <article
+                key={doc.id}
+                className="mb-2 rounded border-2 border-gray-300 p-1 break-all"
+              >
+                <strong>{doc.fullname}</strong>
+                <br />
+                Date of birth: {String(doc.dateofbirth)}
+                <br />
+                Phone number: {String(doc.phonenumber)}
+                <br />
+                E-mail: {String(doc.email)}
+                <br />
+                {/* TODO: format registration date
+                <code className="block text-sm">
+                  Registration date: {String(new Date(doc.timestamp))}
+                </code>*/}
+                <code className="block text-sm">Registration ID: {doc.id}</code>
+              </article>
+            )
+          })}
+        </section>
       </div>
     </main>
   )
